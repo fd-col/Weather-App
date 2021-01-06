fafe61710f6a8b38c7f8e9a64b60d8e4445b1d02
fd-col <s1093242@studenti.univpm.it>
2021-01-06 12:12:56 +0100

    creazione classe Database.java

---
+++ ProgettoOOP/src/main/java/it/univpm/progetto/controller/Controller.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package it.univpm.progetto.controller;
+
+import java.net.URISyntaxException;
+
+import org.json.simple.JSONObject;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import it.univpm.progetto.model.Database;
+import it.univpm.progetto.model.WeatherData;
+
+/**
+ * @author colleluori
+ * @author camplese
+ *
+ */
+@RestController 
+public class Controller {
+
+	
+	@GetMapping (value= "/data")
+	public  JSONObject givaMeData() throws URISyntaxException {
+		WeatherData wd = new WeatherData("Trieste");
+		return wd.print();
+	}
+	
+	@GetMapping(value= "/salva")
+	public void prova() {
+		Database db1 = new Database(); //OPPURE UNA @COMPONENT PER LA CLASSE DATABASE,E PROVARE A CHIAMARE DA LI salveFile
+		Database db2 = new Database();
+		Database db3 = new Database();
+		db1.salvaFile("Trieste", "dati_storici.json");
+		db2.salvaFile("Ortona", "dati_storici.json");
+		db3.salvaFile("Venezia", "dati_storici.json");
+	}
+	
+}
---
+++ ProgettoOOP/src/main/java/it/univpm/progetto/model/WeatherParser.java
@@ -0,0 +1,121 @@
+/**
+ * 
+ */
+package it.univpm.progetto.model;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+
+/**
+ * @author colleluori
+ * @author camplese
+ */
+public class WeatherParser {
+
+	private String cityName;
+	private Long date;
+	private Long visibility;
+	private Double speed;
+	
+	//AppidFromFile appid = new AppidFromFile(); // non va bene????
+	
+	/**
+	 * Costruttore della classe Parser
+	 * @param cityName
+	 */
+	public WeatherParser(String cityName) {
+		this.cityName = cityName;
+	}
+	/**
+	 * @return the cityName
+	 */
+	public String getCityName() {
+		return cityName;
+	}
+	/**
+	 * @return the date
+	 */
+	public Long getDate() {
+		return date;
+	}
+	/**
+	 * @return the visibility
+	 */
+	public Long getVisibility() {
+		return visibility;
+	}
+	/**
+	 * @return the speed
+	 */
+	public Double getSpeed() {
+		return speed;
+	}
+	/**
+	 * prende l'API key da file
+	 * @return una stringa contenente l'API key presa da file
+	 * @throws FileNotFoundException
+	 */
+	public String appidFromFile() throws FileNotFoundException {
+
+		char[] appidChar = new char[32];
+		int i=0;
+		try {
+			int next;
+			BufferedReader reader = new BufferedReader(new FileReader("appid.txt"));
+			do {
+				next = reader.read();
+				if(next!=-1) {
+					char c = (char) next;
+					appidChar[i] = c;
+					i++;
+				}
+			}while(next!=-1);
+			//converto appidChar da char[] ad una String
+
+			reader.close();
+		}catch(IOException e) {
+			System.out.println(e);
+		}
+		return new String(appidChar);
+	}
+	/**
+	 * metodo che effettua il parsing dell'JSON ritornato dalla chiamata a OpenWeather
+	 * @throws ParseException
+	 * @throws FileNotFoundException 
+	 * @throws RestClientException 
+	 * @throws URISyntaxException 
+	 */
+	public void parseMethod() throws ParseException, RestClientException, FileNotFoundException, URISyntaxException {
+		JSONParser parser = new JSONParser();
+		JSONObject obj = new JSONObject();
+		
+		URI uri = new URI("http://api.openweathermap.org/data/2.5/weather?q="+this.cityName+
+						  "&appid="+appidFromFile());
+		RestTemplate restTemplate = new RestTemplate();
+		String jsonResponse = restTemplate.getForObject(uri, String.class);
+		try {
+			obj = (JSONObject) parser.parse(jsonResponse);
+			this.cityName = (String) obj.get("name");
+			this.date = (Long) obj.get("dt");	
+			this.visibility = (Long) obj.get("visibility");
+			JSONObject wind = (JSONObject) obj.get("wind");
+			this.speed = (Double) Double.parseDouble( wind.get("speed").toString() );
+		} catch(ParseException e){
+			e.printStackTrace();		
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
