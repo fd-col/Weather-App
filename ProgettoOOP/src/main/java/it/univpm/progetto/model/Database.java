10e02c8ae913412ccf6ce2a8819dc88d58152594
fd-col <s1093242@studenti.univpm.it>
2021-01-06 12:12:23 +0100

    ultime modifiche 06/07/2021 creazione classe Database.java

---
+++ ProgettoOOP/src/main/java/it/univpm/progetto/model/Database.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package it.univpm.progetto.model;
+
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URISyntaxException;
+
+import org.springframework.stereotype.Component;
+
+
+/**
+ * accumula dati su file locale dalle API di OpenWeather
+ * @author fedju
+ *
+ */
+@Component
+public class Database {
+	/**
+	 * Metodo per salvare un oggetto in un file di testo .json.
+	 * Posso scegliere se salvare un JSONObject oppure un JSONArray.
+	 * 
+	 * @param nome_file Nome del file in cui salvare l'oggetto.
+	 * @param isObject Specifica se l'oggetto da salvare è un JSONObject oppure un JSONArray.
+	 */
+	public void salvaFile(String cityName, String nf) {
+		String nome_file = nf;
+		try {
+			WeatherData wd = new WeatherData(cityName);
+			PrintWriter file_output = new PrintWriter(
+										new BufferedWriter(
+											new FileWriter(nome_file, true)));
+			file_output.println(wd.print());
+			file_output.close();
+			System.out.println("File salvato!"); //PER VERIFICA
+		} catch (IOException | URISyntaxException e) {
+			e.printStackTrace();
+		}
+	}
+
+	
+
+}
